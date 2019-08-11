05eec7e58fdd85ad72960db163e128aed767aca2
Michel Atieno <mishqamish@gmail.com>
2019-08-11 11:43:30 +0300

    Set up project

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/michel/Desktop/100DaysOfCode/Mishqa_Rogue_Nation/django_project_boilerplate-master/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
