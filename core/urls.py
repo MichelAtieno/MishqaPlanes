3d3b6c9a1e0f53920ac707e4140f14abf76619f5
Michel Atieno <mishqamish@gmail.com>
2019-08-13 13:31:30 +0300

    Successfully added coupon and user is able to complete transaction from adding to cart to making payment

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, CheckoutView, ItemDetailView, add_to_cart, remove_from_cart, OrderSummaryView,  remove_single_item_from_cart, PaymentView
+from .views import HomeView, CheckoutView, ItemDetailView, add_to_cart, remove_from_cart, OrderSummaryView,  remove_single_item_from_cart, PaymentView, AddCouponView
 
 app_name="core"
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('order-summary', OrderSummaryView.as_view(), name="order-summary"),
     path('product/<slug>/', ItemDetailView.as_view(), name="product"),
     path('add-to-cart/<slug>/', add_to_cart, name="add-to-cart"),
+    path('add-coupon/', AddCouponView.as_view(), name="add-coupon"),
     path('remove-from-cart/<slug>/', remove_from_cart, name="remove-from-cart"),
     path('remove-item-from-cart/<slug>/',remove_single_item_from_cart, name="remove-single-item-from-cart"),
     path('payment/<payment_option>/', PaymentView.as_view(), name="payment")
