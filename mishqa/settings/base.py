abcfa4a446039dfe37a3b93b58a5f2126c2b4e7a
Michel Atieno <mishqamish@gmail.com>
2019-08-13 12:22:57 +0300

    Fixed ordered error. Adding single order item

--- mishqa/settings/base.py
+++ mishqa/settings/base.py
@@ -64,9 +64,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(VENV_PATH, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
